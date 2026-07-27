6ef3e1e7b1a6e578d4b12853571471f6578f1d8a
Taha <taha@gmail.com>
2026-07-27 22:07:18 +0500

    push

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance with base URL and credentials
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
